2d5b6561d2cca7278768b2a83dba7e21dee5a8bd
mikeycurtis <59662812+mikeycurtis@users.noreply.github.com>
2021-05-29 15:41:44 -0700

    Update getPrediction.py

--- PredictionApp/getPrediction.py
+++ PredictionApp/getPrediction.py
@@ -7,8 +7,8 @@ import pandas as pd
 
 
 def getPrediction(input_arr):
-    yup = []
-    yup.append(input_arr)
+    inputs = []
+    inputs.append(input_arr)
     model = tf.keras.models.load_model(
         '../finalModelNBA')
-    return(model.predict(yup)[0][0])
+    return(model.predict(inputs)[0][0])
